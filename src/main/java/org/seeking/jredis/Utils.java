2eda8c01d24d553d06241cf9e90355598a0b0848
Andy HaoQi Li <andy.li@movit-tech.com>
2017-11-03 16:56:53 +0800

    init

---
+++ src/main/java/org/seeking/jredis/Utils.java
@@ -0,0 +1,11 @@
+package org.seeking.jredis;
+
+public class Utils {
+    public long CRC16(String string) {
+        return 0L;
+    }
+
+    public long CRC64(String string) {
+        return 0L;
+    }
+}
