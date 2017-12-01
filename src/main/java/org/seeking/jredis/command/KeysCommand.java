c6db90aada497d395a111d0da8911d5e8ddaba90
Andy HaoQi Li <andy.li@movit-tech.com>
2017-12-01 13:33:43 +0800

    keys command

--- src/main/java/org/seeking/jredis/command/KeysCommand.java
+++ src/main/java/org/seeking/jredis/command/KeysCommand.java
@@ -2,9 +2,13 @@ package org.seeking.jredis.command;
 
 import org.seeking.jredis.Command;
 import org.seeking.jredis.Reply;
+import org.seeking.jredis.reply.BulkReply;
+import org.seeking.jredis.reply.MultiBulkReply;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.regex.Pattern;
 
 public class KeysCommand implements Command {
     private Map<String, Object> memory;
@@ -15,7 +19,13 @@ public class KeysCommand implements Command {
 
     @Override
     public Reply eval(List<String> params) {
-        // TODO
-        return null;
+        String pattern = params.get(0).replace("*", ".*").replace("?", ".?");
+        List<Reply> list = new ArrayList<>();
+        for (String key : memory.keySet()) {
+            if (Pattern.matches(pattern, key)) {
+                list.add(new BulkReply(key));
+            }
+        }
+        return new MultiBulkReply(list);
     }
 }
