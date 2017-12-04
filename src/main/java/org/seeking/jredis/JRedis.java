60695f473f956d0ad1b6490885f7efe8fbbcd574
Andy HaoQi Li <andy.li@movit-tech.com>
2017-12-04 10:04:37 +0800

    argument parser

--- src/main/java/org/seeking/jredis/JRedis.java
+++ src/main/java/org/seeking/jredis/JRedis.java
@@ -1,22 +1,47 @@
 package org.seeking.jredis;
 
+import org.apache.commons.cli.*;
 import org.apache.mina.core.service.IoAcceptor;
 import org.apache.mina.core.session.IdleStatus;
 import org.apache.mina.filter.codec.ProtocolCodecFilter;
-import org.apache.mina.filter.logging.LoggingFilter;
 import org.apache.mina.transport.socket.nio.NioSocketAcceptor;
 
-import java.io.IOException;
 import java.net.InetSocketAddress;
 
 public class JRedis {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
+        CommandLineParser parser = new DefaultParser();
+        Options options = new Options();
+        options.addOption("h", "help", false, "Print usage information");
+        options.addOption("p", "port", true, "Server port (default: 9000)");
+        CommandLine commandLine;
+        try {
+            commandLine = parser.parse(options, args);
+        } catch (ParseException ex) {
+            printHelp(options);
+            return;
+        }
+
+        if (commandLine.hasOption("h")) {
+            printHelp(options);
+            return;
+        }
+
+        int port = 9000;
+        if (commandLine.hasOption("p")) {
+            port = Integer.valueOf(commandLine.getOptionValue("p"));
+        }
+
         IoAcceptor acceptor = new NioSocketAcceptor();
-        acceptor.getFilterChain().addLast("logger", new LoggingFilter());
         acceptor.getFilterChain().addLast("codec", new ProtocolCodecFilter(new JRedisCodecFactory()));
         acceptor.setHandler(new JRedisHandler());
         acceptor.getSessionConfig().setReadBufferSize(2048);
         acceptor.getSessionConfig().setIdleTime(IdleStatus.BOTH_IDLE, 10);
-        acceptor.bind(new InetSocketAddress(9000));
+        acceptor.bind(new InetSocketAddress(port));
+    }
+
+    static void printHelp(Options options) {
+        HelpFormatter hf = new HelpFormatter();
+        hf.printHelp("Options", options);
     }
 }
